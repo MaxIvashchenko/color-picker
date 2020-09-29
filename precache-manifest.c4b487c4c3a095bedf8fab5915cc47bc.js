bfbe238ed5c3eb3919ae80eb0e7dd3502630b73f
Ivashchenko Maksym <maksym.entex@gmail.com>
2020-09-29 16:11:26 +0300

    Updates

---
+++ precache-manifest.c4b487c4c3a095bedf8fab5915cc47bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49508e493a3165383c9187cf3a3c063f",
+    "url": "/color-picker/index.html"
+  },
+  {
+    "revision": "54a1e4b3679a2dccd96a",
+    "url": "/color-picker/static/css/main.77f75c24.chunk.css"
+  },
+  {
+    "revision": "629055ff9a7a0184a7d1",
+    "url": "/color-picker/static/js/2.b6749d7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/color-picker/static/js/2.b6749d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54a1e4b3679a2dccd96a",
+    "url": "/color-picker/static/js/main.6d1bd436.chunk.js"
+  },
+  {
+    "revision": "9f2f1b83fd3ea7a8bb90",
+    "url": "/color-picker/static/js/runtime-main.ac77659b.js"
+  },
+  {
+    "revision": "077044301bf178992218466e50d4886f",
+    "url": "/color-picker/static/media/sort-down.07704430.svg"
+  }
+]);
